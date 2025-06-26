c902be97d30dcd1c9fded543ca1c44f42aa1b77c
Yashasvi <yashasvikumar960@gmail.com>
2025-06-25 21:17:28 -0400

    feat(autoSuggest): adding fuzzySearch

--- src/main/java/Trie/Trie.java
+++ src/main/java/Trie/Trie.java
@@ -1,5 +1,7 @@
 package Trie;
 
+import java.util.*;
+
 public class Trie {
     public  TrieNode root;
 
@@ -9,7 +11,7 @@ public class Trie {
 
     public void insert(String word, int freq) {
         TrieNode node = root;
-        for(char c: word.toCharArray()) {
+        for(char c: word.toLowerCase().toCharArray()) {
             if(node.children[c-'a']==null) {
                 node.children[c-'a']=new TrieNode();
             }
@@ -17,5 +19,75 @@ public class Trie {
         }
         node.isEndOfWord = true;
         node.wordFrequency += freq;
+    }
+
+    public List<String> getSuggestionsWithFuzzyPrefix(String input, int k) {
+        TrieNode node = root;
+        PriorityQueue<String[]> pq = new PriorityQueue<>((a,b) -> {
+            int freqMap = Integer.compare(Integer.parseInt(a[1]), Integer.parseInt(b[1]));
+            return freqMap!=0?freqMap:b[0].compareTo(a[0]);
+        });
+       Set<String> seen = new HashSet<>();
+        fuzzyPrefixSearch(node, input.toLowerCase(), "", k, pq, 2, seen);
+        List<String> result = new ArrayList<>();
+        while(!pq.isEmpty()) {
+            result.add(0, pq.poll()[0]);
+        }
+
+        return result;
+    }
+
+    public void fuzzyPrefixSearch(TrieNode node, String input, String currPrefix,
+                                  int k, PriorityQueue<String[]> pq,
+                                  int maxDistance, Set<String> seen) {
+        if(editDistance(input, currPrefix)<=maxDistance) {
+          collectWords(node, currPrefix, k, pq, seen);
+        }
+
+        for(char first = 'a'; first<='z'; first++) {
+            if(node.children[first-'a']!=null) {
+                fuzzyPrefixSearch(node.children[first-'a'], input, currPrefix+first, k, pq, maxDistance, seen);
+            }
+        }
+    }
+
+    public int editDistance(String input1, String input2) {
+        int m1 = input1.length();
+        int m2 = input2.length();
+
+        int dp[][] = new int[m1+1][m2+1];
+        for(int i = 0; i<m1+1; i++) {
+            dp[i][0] = i;
+        }
+        for(int j = 0;j<m2+1; j++) {
+            dp[0][j] = j;
+        }
+
+        for(int i =1 ; i<m1+1; i++) {
+            for(int j =1; j<m2+1; j++) {
+                if(input1.charAt(i-1)==input2.charAt(j-1)) {
+                    dp[i][j] = dp[i-1][j-1];
+                } else{
+                    dp[i][j] = 1+Math.min(dp[i-1][j], Math.min(dp[i][j-1], dp[i-1][j-1]));
+                }
+            }
+        }
+
+        return dp[m1][m2];
+    }
+
+    public void collectWords(TrieNode node, String currPrefix,
+                                  int k, PriorityQueue<String[]> pq, Set<String> seen){
+      if(node.isEndOfWord && !seen.contains(currPrefix)) {
+          pq.offer(new String[] {currPrefix, String.valueOf(node.wordFrequency)});
+          seen.add(currPrefix);
+          if(pq.size()>k) pq.poll();
+      }
+
+      for(char first = 'a'; first<='z'; first++) {
+          if(node.children[first-'a']!=null) {
+              collectWords(node.children[first-'a'],currPrefix+first, k, pq, seen);
+          }
+      }
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,20 @@
 package org.example;
 
+import Trie.Trie;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Trie trie  = new Trie();
+        trie.insert("java", 10);
+        trie.insert("javascript", 15);
+        trie.insert("javadoc", 5);
+        trie.insert("json", 8);
+        trie.insert("jam", 3);
+
+        List<String> suggestions = trie.getSuggestionsWithFuzzyPrefix("Jsvs", 3);
+        System.out.println(suggestions);
     }
 }
